24e5334340fd4786c17235e41d1f282a1b88e5e5
Amal Raj <99964089+amalraj28@users.noreply.github.com>
2022-05-22 03:06:04 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-import string
+import string   # To generate list of alphabets without typing each of them manually
 import random
 
 print('Welcome to the password generator!!!')
@@ -11,9 +11,9 @@ count_letters = int(input('How many letters in the password?: '))
 count_symbol = int(input('Enter number of symbols: '))
 count_num = int(input('Enter number of numbers: '))
 
-random_sym = random.choices(symbol, k=count_symbol)    # Generates a randomly generated list of length k from the list symbol
-random_num = random.choices(number_str, k=count_num)  # Generates a randomly generated list of length k from the list number_str
-random_alpha = random.choices(alphabet, k=count_letters)  # Generates a randomly generated list of length k from the list alphabet
+random_sym = random.choices(symbol, k=count_symbol)    # Generates a random list of length k from the list symbol
+random_num = random.choices(number_str, k=count_num)  # Generates a random list of length k from the list number_str
+random_alpha = random.choices(alphabet, k=count_letters)  # Generates a random list of length k from the list alphabet
 
 final_list = list()         # This will be the list formed by combining the above 3 randomly generated lists
 final_list.extend(random_alpha)
@@ -22,6 +22,6 @@ final_list.extend(random_sym)
 
 random.shuffle(final_list)  # Shuffles the combined list to make it totally random with regard to position of alphabets, numbers and symbols
 
-password = "".join(final_list)  # Converts the list to password
+password = "".join(final_list)  # Converts the list to string
 
 print(f'Password: {password}')
